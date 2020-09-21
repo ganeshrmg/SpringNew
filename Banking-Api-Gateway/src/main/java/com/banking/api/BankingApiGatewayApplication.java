fc9452a6acb2b5836758bf5b5d4a308abdda655a
ganeshrmg <69802909+ganeshrmg@users.noreply.github.com>
2020-09-21 23:03:35 +0530

    Add files via upload

---
+++ Banking-Api-Gateway/src/main/java/com/banking/api/BankingApiGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.banking.api;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class BankingApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingApiGatewayApplication.class, args);
+	}
+
+}
---
+++ Banking-Api-Gateway/src/main/java/com/banking/api/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package com.banking.api;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.httpBasic()
+		.and()
+			.authorizeRequests()
+				.antMatchers("/", "/home","/duediligence/getDuediligenceStatus/*","/getDuediligenceStatus/*").permitAll()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.loginPage("/loginpage")
+				.permitAll()
+				.and()
+			.logout()
+				.permitAll();
+	}
+
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		UserDetails user =
+			 User.withDefaultPasswordEncoder()
+				.username("Ganesh")
+				.password("pass")
+				.roles("USER")
+				.build();
+
+		return new InMemoryUserDetailsManager(user);
+	}
+}
